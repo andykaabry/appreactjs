53e12e45151f07e121da3226ccb57b60170230f3
andyka.abry <andyka.abry@outlook.com>
2020-06-19 13:55:07 +0700

    State pada React js

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,27 +1,16 @@
 import React from 'react'
-import Blog from './Blog'
+import Produk from '../Class/Produk';
+//import Blog from './Blog'
 
 const Home = () => {
     return <div>
-        <Blog
-            tanggal="19 Juni 2020"
-            judul="Teknologi Blockchain"
-            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
-        />
-
-        <Blog
-            tanggal="20 Juni 2020"
-            judul="Teknologi Internet Of Things"
-            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
-        />
-
-        <Blog
-            tanggal="21 Juni 2020"
-            judul="Design Pattern Laravel"
-            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
-        />
-
+    <Produk nama="Asus ROG 2020" stock="10" harga="24000000"/>
+    <Produk nama="Asus ROG 2019" stock="15" harga="23000000"/>
+    <Produk nama="Asus ROG 2018" stock="12" harga="21000000"/>
+    <Produk nama="Asus ROG 2017" stock="19" harga="19000000"/>
+    <Produk nama="Asus ROG 2016" stock="17" harga="18000000"/>
+    <Produk nama="Asus ROG 2015" stock="14" harga="17000000"/>
     </div>
-}
 
+}
 export default Home;
