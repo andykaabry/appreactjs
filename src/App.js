fb05d34eefdab14c9c68e580a0dacfc0eb495505
andyka.abry <andyka.abry@outlook.com>
2020-06-20 16:45:48 +0700

    React Router dengan Passing Data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavbarComp from './Component/Fungsional/NavbarComp';
 import HomePage from './Component/Fungsional/HomePage';
 import About from './Component/Fungsional/AboutComp';
+import DetailComp from './Component/Fungsional/DetailComp';
 
 // import BootstrapComp from './Component/Class/BootstrapComp';
 //import Parent from './Component/Class/Parent';
@@ -18,6 +19,7 @@ const App = () => {
       <switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/detail/:id" component={DetailComp} />
       </switch>
     </BrowserRouter>
   );
