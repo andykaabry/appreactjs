0224097e644cc727bd11633d8b24be032cd24e8c
andyka.abry <andyka.abry@outlook.com>
2020-06-20 09:58:15 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import BootstrapComp from './Component/Class/BootstrapComp';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
+
+// import BootstrapComp from './Component/Class/BootstrapComp';
 //import Parent from './Component/Class/Parent';
 // import logo from './logo.svg';
 // import './App.css';
 // import Home from './Component/Fungsional/Home';
 // import Beranda from './Component/Class/Beranda';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <BootstrapComp/>
-    </div>
+    <BrowserRouter>
+      <NavbarComp/>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+      </switch>
+    </BrowserRouter>
   );
 }
 
